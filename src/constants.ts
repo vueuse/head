3af84f707e2751ade3830a1f42d481d3398ed5f1
EGOIST <0x142857@gmail.com>
2021-02-16 13:59:01 +0800

    refactor: tweak PROVIDE_KEY

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,4 @@
-export const PROVIDE_KEY = Symbol('@vueuse/head')
+export const PROVIDE_KEY = `usehead`
 export const HEAD_COUNT_KEY = `head:count`
 // Store attr names for `htmlAttrs`, `bodyAttrs` so we can remove them first before next update
 export const HEAD_ATTRS_KEY = `data-head-attrs`
