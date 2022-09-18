3949c194ff38a3bff28cd76e502f7c3d216b91b6
Harlan Wilton <harlan@harlanzw.com>
2022-09-18 22:53:41 +1000

    chore: maybe fix module resolution bug

--- examples/nuxt3/nuxt.config.ts
+++ examples/nuxt3/nuxt.config.ts
@@ -2,13 +2,14 @@ import { defineNuxtConfig } from "nuxt/config"
 import { fileURLToPath } from "url"
 
 const runtimeDir = fileURLToPath(new URL("./runtime", import.meta.url))
+const rootDir = fileURLToPath(new URL("../../", import.meta.url))
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
   alias: {
-    "@vueuse/head": fileURLToPath(new URL("../../", import.meta.url)),
+    "@vueuse/head": `${rootDir}/src`,
   },
-  workspaceDir: fileURLToPath(new URL("../../", import.meta.url)),
+  workspaceDir: rootDir,
   hooks: {
     "modules:done"({ nuxt }) {
       // Replace #head alias
