c2a892edf9043ce9c8ac5a4ee48746909b912002
Harlan Wilton <harlan@harlanzw.com>
2022-10-01 13:50:44 +1000

    chore: improve nuxt3 tests / example

---
+++ examples/nuxt3/components/ModifyTitle.vue
@@ -0,0 +1,13 @@
+<script lang="ts" setup>
+const props = defineProps<{
+  title: string
+}>()
+await new Promise((resolve) => setTimeout(resolve, 1000))
+
+useHead({
+  title: computed(() => props.title),
+})
+</script>
+<template>
+  <div>final title test</div>
+</template>
---
+++ examples/nuxt3/runtime/plugin.ts
@@ -0,0 +1,75 @@
+import { createHead, renderHeadToString } from "@vueuse/head"
+import {
+  computed,
+  ref,
+  watchEffect,
+  onBeforeUnmount,
+  getCurrentInstance,
+  ComputedGetter,
+} from "vue"
+import defu from "defu"
+import type { MetaObject } from "."
+import { defineNuxtPlugin, useRoute } from "#app"
+import { useRouter, watch } from "#imports"
+
+// Note: This is just a copy of Nuxt's internal head plugin with modifications made for this issue
+
+export default defineNuxtPlugin((nuxtApp) => {
+  const head = createHead()
+
+  nuxtApp.vueApp.use(head)
+
+  const headReady = ref(false)
+  nuxtApp.hooks.hookOnce("app:mounted", () => {
+    watchEffect(() => {
+      head.updateDOM()
+    })
+    headReady.value = true
+  })
+
+  nuxtApp._useHead = (_meta: MetaObject | ComputedGetter<MetaObject>) => {
+    const meta = ref<MetaObject>(_meta)
+    const headObj = computed(() => {
+      const overrides: MetaObject = { meta: [] }
+      if (meta.value.charset) {
+        overrides.meta!.push({ key: "charset", charset: meta.value.charset })
+      }
+      if (meta.value.viewport) {
+        overrides.meta!.push({ name: "viewport", content: meta.value.viewport })
+      }
+      return defu(overrides, meta.value)
+    })
+    head.addHeadObjs(headObj as any)
+
+    if (process.server) {
+      return
+    }
+
+    if (headReady) {
+      watchEffect(() => {
+        head.updateDOM()
+      })
+    }
+
+    const vm = getCurrentInstance()
+    if (!vm) {
+      return
+    }
+
+    onBeforeUnmount(() => {
+      head.removeHeadObjs(headObj as any)
+      head.updateDOM()
+    })
+  }
+
+  if (process.server) {
+    nuxtApp.ssrContext!.renderMeta = () => {
+      const meta = renderHeadToString(head)
+      return {
+        ...meta,
+        // resolves naming difference with NuxtMeta and @vueuse/head
+        bodyScripts: meta.bodyTags,
+      }
+    }
+  }
+})
