c1d38ae1871a7f84f51bd0e028826298390e7e14
Harlan Wilton <harlan@harlanzw.com>
2022-10-15 10:06:08 +1100

    fix: ensure tags props are immutable (#139)

---
+++ examples/nuxt3/pages/issues/8181/test.vue
@@ -0,0 +1,19 @@
+<template>
+<div>
+  <div class="red">Test Page</div>
+  <NuxtLink to="/issues/8181/test">test</NuxtLink>
+  <br />
+  <NuxtLink to="/issues/8181">index</NuxtLink>
+</div>
+</template>
+
+<script setup lang="ts">
+useHead({
+  style: [
+    {
+      id: 'test',
+      children: '.red { background-color: red }',
+    },
+  ],
+});
+</script>
