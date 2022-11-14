a124b7b5ef58cd31e7987d268a6d44ffd7e54ae9
Harlan Wilton <harlan@harlanzw.com>
2022-11-14 18:14:45 +1100

    fix: export all `@unhead/vue` composables

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,33 @@ export * from './createHead'
 export * from './legacy'
 export * from './components'
 export type { Unhead, HeadEntryOptions, MaybeComputedRef, UseHeadInput, MergeHead, ReactiveHead } from '@unhead/vue'
-export { useHead, useServerHead, VueHeadMixin } from '@unhead/vue'
+// export opt-in utils
+export { VueHeadMixin, unheadVueComposablesImports, Vue2ProvideUnheadPlugin } from '@unhead/vue'
+// export composables
+export {
+  injectHead,
+  useHead,
+  useTagTitle,
+  useTagBase,
+  useTagMeta,
+  useTagMetaFlat,
+  useTagLink,
+  useTagScript,
+  useTagStyle,
+  useTagNoscript,
+  useHtmlAttrs,
+  useBodyAttrs,
+  useTitleTemplate,
+  useServerHead,
+  useServerTagTitle,
+  useServerTagBase,
+  useServerTagMeta,
+  useServerTagMetaFlat,
+  useServerTagLink,
+  useServerTagScript,
+  useServerTagStyle,
+  useServerTagNoscript,
+  useServerHtmlAttrs,
+  useServerBodyAttrs,
+  useServerTitleTemplate,
+} from '@unhead/vue'
