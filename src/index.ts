cfcab52938f07c1b5d9b87321720ebdaae79e699
Harlan Wilton <harlan@harlanzw.com>
2022-11-14 22:57:27 +1100

    types: expose `ActiveHeadEntry`

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 export * from './createHead'
 export * from './legacy'
 export * from './components'
-export type { Unhead, HeadEntryOptions, MaybeComputedRef, UseHeadInput, MergeHead, ReactiveHead, HeadTag } from '@unhead/vue'
+export type { Unhead, HeadEntryOptions, MaybeComputedRef, UseHeadInput, MergeHead, ReactiveHead, HeadTag, ActiveHeadEntry } from '@unhead/vue'
 // export opt-in utils
 export { VueHeadMixin, unheadVueComposablesImports, Vue2ProvideUnheadPlugin, createHeadCore } from '@unhead/vue'
 // export composables
